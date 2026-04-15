677504740c54ec772b02096f2ad8ff5855148182
theauclt <theaunguyenc@gmail.com>
2026-04-15 09:33:39 +0000

    modif stats service

--- pkg/services/stats_service.py
+++ pkg/services/stats_service.py
@@ -70,3 +70,55 @@ class StatsService:
         if entity_name in self.stats:
             return self.stats[entity_name]
         return None
+    
+    # --- MÉTHODES DE MANIPULATION COMPLEXES ---
+
+    def get_team_history(self, team_name):
+        """
+        Filtre la base de données de matchs pour ne ressortir que l'historique d'une équipe.
+        Utilise la nouvelle méthode `involves_team` du modèle Match.
+        """
+        return [match for match in self.matches if match.involves_team(team_name)]
+
+    def calculate_standings(self, points_for_win=3, points_for_draw=1, points_for_loss=0):
+        """
+        Calcule le classement générique d'un tournoi (type championnat).
+        Retourne un dictionnaire complexe avec toutes les stats agrégées.
+        """
+        standings = defaultdict(lambda: {'played': 0, 'points': 0, 'wins': 0, 'draws': 0, 'losses': 0, 'pf': 0, 'pa': 0, 'diff': 0})
+
+        for match in self.matches:
+            t1, t2 = match.team1, match.team2
+            
+            standings[t1]['played'] += 1
+            standings[t2]['played'] += 1
+            
+            # Utilisation des méthodes intelligentes du modèle Match
+            standings[t1]['pf'] += match.get_points_for(t1)
+            standings[t1]['pa'] += match.get_points_against(t1)
+            standings[t2]['pf'] += match.get_points_for(t2)
+            standings[t2]['pa'] += match.get_points_against(t2)
+
+            winner = match.get_winner()
+            if winner == t1:
+                standings[t1]['wins'] += 1
+                standings[t1]['points'] += points_for_win
+                standings[t2]['losses'] += 1
+                standings[t2]['points'] += points_for_loss
+            elif winner == t2:
+                standings[t2]['wins'] += 1
+                standings[t2]['points'] += points_for_win
+                standings[t1]['losses'] += 1
+                standings[t1]['points'] += points_for_loss
+            else:
+                standings[t1]['draws'] += 1
+                standings[t2]['draws'] += 1
+                standings[t1]['points'] += points_for_draw
+                standings[t2]['points'] += points_for_draw
+
+        # Calcul de la différence de points finale
+        for team, stats in standings.items():
+            stats['diff'] = stats['pf'] - stats['pa']
+
+        # Tri : Points > Différence de points > Points marqués
+        return sorted(standings.items(), key=lambda x: (x[1]['points'], x[1]['diff'], x[1]['pf']), reverse=True)
