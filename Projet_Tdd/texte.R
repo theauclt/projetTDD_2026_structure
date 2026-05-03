389087084a8d0670a86ff264273d46f2647bab8d
LASSALLE Gwenael <id3084@clust-n2.domensai.ecole>
2026-05-03 11:28:58 +0200

    maj

---
+++ Projet_Tdd/texte.R
@@ -0,0 +1,29 @@
+* **Tennis** :
+  * `atp_matches_2024.csv` : Résultats détaillés des matchs du circuit ATP.
+* `atp_players_2024.csv` : Informations sur les joueurs du circuit ATP.
+* `wta_matches_2024.csv` : Résultats détaillés des matchs du circuit WTA.
+* `wta_players_2024.csv` : Informations sur les joueuses du circuit WTA.
+
+* **Badminton** :
+  * `match.csv` : Données sur les matchs.
+* `player.csv` : Informations sur les joueurs.
+
+* **Basket-ball** :
+  * `game.csv` : Données sur les matchs NBA.
+* `player.csv` : Informations sur les joueurs NBA.
+* `team.csv` : Informations sur les équipes NBA.
+
+* **Football Champions League** :
+  * `match.csv` : 
+  * `player.csv` : 
+  * `team.csv` : 
+  
+  * **Volley-ball** :
+  * `coach_men.csv`: Informations sur les coachs des équipes masculines
+* `coach_women.csv`: Informations sur les coachs des équipes féminines
+* `country.csv`
+* `match_men.csv`
+* `match_women.csv`
+* `player_men.csv`
+* `player_women.csv`
+
